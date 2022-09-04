09efd0ea227a52526eadb22b290826c9792be654
Jordie Guzman <jordieyguzman@gmail.com>
2022-09-04 10:04:27 -0700

    Change println to print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,7 +17,7 @@ public class Main {
       BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
       PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
       //RespResponse response = new RespResponse("PONG", RespResponse.RespResponseType.SIMPLE_STRING);
-      out.println("+PONG\r\n");
+      out.print("+PONG\r\n");
       while (true) {
         out.print("+PONG\r\n");
       }

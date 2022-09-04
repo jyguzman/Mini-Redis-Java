6dab60629195c09300003f0716f1a2658e888f4c
Jordie Guzman <jordieyguzman@gmail.com>
2022-09-04 08:56:00 -0700

    Pass first stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
@@ -25,7 +25,6 @@ public class Main {
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         }
-      }
     }
   }
 }

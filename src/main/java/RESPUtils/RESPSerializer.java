39737436f567ae28bcca7911a75e2a2d8f5f0528
Jordie Guzman <jordieyguzman@gmail.com>
2022-09-06 10:38:41 -0700

    Fix some errors

--- src/main/java/RESPUtils/RESPSerializer.java
+++ src/main/java/RESPUtils/RESPSerializer.java
@@ -5,8 +5,24 @@ import java.util.Arrays;
 import java.util.stream.*;
 public class RESPSerializer {
     private static final String CLRF = "\r\n";
-    public String serializeToBulkString(String message) {
-        return "$" + message.length() + CLRF + message + CLRF;
+
+    public String serializeString(String message, String type) {
+        char firstByte = 'a';
+        switch (type) {
+            case "BulkString":
+                firstByte = '$';
+                break;
+            case "SimpleString":
+                firstByte = '+';
+                break;
+            case "Error":
+                firstByte = '-';
+                break;
+            case "Integer":
+                firstByte = ':';
+                break;
+        }
+        return firstByte + message.length() + CLRF + message + CLRF;
     }
 
     public String serializeToRespArray(String command) {
@@ -14,7 +30,7 @@ public class RESPSerializer {
         StringBuilder respArray = new StringBuilder();
         respArray.append("*" + clientMessageArgs.size() + CLRF);
         String joinedBulkStrings = clientMessageArgs.stream()
-                .map(string -> this.serializeToBulkString(string))
+                .map(string -> this.serializeString(string, "BulkString"))
                 .collect(Collectors.joining(""));
         return respArray.append(joinedBulkStrings).toString();
     }
@@ -22,8 +38,8 @@ public class RESPSerializer {
     public static void main(String[] args) {
         String x = "ECHO hey";
         RESPSerializer ser = new RESPSerializer();
-        System.out.println(ser.serializeToBulkString("hello"));
-        System.out.println(ser.serializeToBulkString("{ \"hello\": 5 }")); 
+        System.out.println(ser.serializeString("hello", "BulkString"));
+        System.out.println(ser.serializeString("{ \"hello\": 5 }", "BulkString"));
         String res = ser.serializeToRespArray(x);
         System.out.println(("*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n").equals(res));
     }
